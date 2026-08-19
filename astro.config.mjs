cf1e243f525669a96ece0e9c986698e788f670cb
Fockus26 <alejandrorey2654@gmail.com>
2026-08-19 19:50:32 -0400

    temp: remove vite-pwa to debug build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import AstroPWA from "@vite-pwa/astro";
+// import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -13,49 +13,49 @@ export default defineConfig({
 				plugins: [["babel-plugin-react-compiler"]],
 			},
 		}),
-		AstroPWA({
-			registerType: "autoUpdate",
-			includeAssets: ["favicon.svg", "favicon.ico"],
-			manifest: {
-				name: "Banderas del Mundo",
-				short_name: "Banderas",
-				description: "Aprende las banderas del mundo",
-				start_url: "/",
-				scope: "/",
-				display: "fullscreen",
-				orientation: "portrait",
-				background_color: "#f1f4f8",
-				theme_color: "#2563eb",
-				lang: "es",
-				icons: [
-					{
-						src: "pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-						purpose: "any",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any",
-					},
-					{
-						src: "maskable-icon-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "maskable",
-					},
-				],
-			},
-			workbox: {
-				globPatterns: ["**/*.{css,js,html,svg,png,ico,woff2}"],
-				navigateFallback: "/",
-			},
-			devOptions: {
-				enabled: false,
-			},
-		}),
+		// AstroPWA({
+		// 	registerType: "autoUpdate",
+		// 	includeAssets: ["favicon.svg", "favicon.ico"],
+		// 	manifest: {
+		// 		name: "Banderas del Mundo",
+		// 		short_name: "Banderas",
+		// 		description: "Aprende las banderas del mundo",
+		// 		start_url: "/",
+		// 		scope: "/",
+		// 		display: "fullscreen",
+		// 		orientation: "portrait",
+		// 		background_color: "#f1f4f8",
+		// 		theme_color: "#2563eb",
+		// 		lang: "es",
+		// 		icons: [
+		// 			{
+		// 				src: "pwa-192x192.png",
+		// 				sizes: "192x192",
+		// 				type: "image/png",
+		// 				purpose: "any",
+		// 			},
+		// 			{
+		// 				src: "pwa-512x512.png",
+		// 				sizes: "512x512",
+		// 				type: "image/png",
+		// 				purpose: "any",
+		// 			},
+		// 			{
+		// 				src: "maskable-icon-512x512.png",
+		// 				sizes: "512x512",
+		// 				type: "image/png",
+		// 				purpose: "maskable",
+		// 			},
+		// 		],
+		// 	},
+		// 	workbox: {
+		// 		globPatterns: ["**/*.{css,js,html,svg,png,ico,woff2}"],
+		// 		navigateFallback: "/",
+		// 	},
+		// 	devOptions: {
+		// 		enabled: false,
+		// 	},
+		// }),
 	],
 	vite: {
 		plugins: [tailwindcss()],
